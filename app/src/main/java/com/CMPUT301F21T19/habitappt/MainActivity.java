e386f9f10dd831fd1fddf1ef58ef0154a684f2a7
AnnoyingCotton <andrewmjculberson@gmail.com>
2021-10-11 12:30:34 -0600

    added empty android app

---
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.CMPUT301F21T19.habitappt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
