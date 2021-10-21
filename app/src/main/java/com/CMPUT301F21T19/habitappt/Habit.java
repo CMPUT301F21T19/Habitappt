5d330c867bf1c7c7b585bf395523db255772caea
Andrew Culberson <46251362+the-rooster@users.noreply.github.com>
2021-10-21 13:03:34 -0600

    Implemented nav bar and fragment switching in main container!

--- app/src/main/java/com/CMPUT301F21T19/habitappt/Habit.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/Habit.java
@@ -6,11 +6,5 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class ProfileActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-
-    }
+public class Habit {
 }
--- app/src/main/java/com/CMPUT301F21T19/habitappt/MainActivity.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/MainActivity.java
@@ -1,16 +1,26 @@
 package com.CMPUT301F21T19.habitappt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Transaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements nav_bar.nav_bar_switch {
 
     public FirebaseFirestore db;
 
+    @NonNull
+    @Override
+    public FragmentManager getSupportFragmentManager() {
+        return super.getSupportFragmentManager();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +33,34 @@ public class MainActivity extends AppCompatActivity {
         //TEST: add to it
         FirestoreOps.testDBAddition(db);
 
+        //start bottom navigation bar!
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.bottom_nav_bar,new nav_bar());
+
+        transaction.commit();
+
 
     }
 
 
+    @Override
+    public void switchFragment(nav_bar.nav_states nav_to) {
 
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
+        System.out.println("SWITCHIN");
+
+        if(nav_to == nav_bar.nav_states.PROFILE){
+            transaction.replace(R.id.main_container,new profile());
+        }
+        else if(nav_to == nav_bar.nav_states.ALL_HABITS){
+            transaction.replace(R.id.main_container,new all_habits());
+        }
+        else{
+            transaction.replace(R.id.main_container,new daily_habits());
+        }
+
+        transaction.commit();
+    }
 }
