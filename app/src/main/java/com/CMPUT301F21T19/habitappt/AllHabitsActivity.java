683b52bdc9eb5a6edf79a6a3540a3945bf15f593
AnnoyingCotton <andrewmjculberson@gmail.com>
2021-10-20 21:02:04 -0600

    implement activity class

--- app/src/main/java/com/CMPUT301F21T19/habitappt/AllHabitsActivity.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/AllHabitsActivity.java
@@ -1,4 +1,14 @@
 package com.CMPUT301F21T19.habitappt;
 
-public class AllHabitsActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AllHabitsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all);
+
+    }
 }
--- app/src/main/java/com/CMPUT301F21T19/habitappt/ProfileActivity.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/ProfileActivity.java
@@ -1,4 +1,16 @@
 package com.CMPUT301F21T19.habitappt;
 
-public class ProfileActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
 }
