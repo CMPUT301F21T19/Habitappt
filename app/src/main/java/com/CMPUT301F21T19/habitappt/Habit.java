0fc3a1efec8e3c9c53bc376fe463645ae0eac1ef
loganv90 <60831705+loganv90@users.noreply.github.com>
2021-10-23 15:10:29 -0600

    add all habits UI

--- app/src/main/java/com/CMPUT301F21T19/habitappt/Habit.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/Habit.java
@@ -7,4 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Habit {
+    private String name;
+
+    Habit(String name){
+        this.name = name;
+    }
+
+    String getName(){
+        return this.name;
+    }
 }
--- app/src/main/java/com/CMPUT301F21T19/habitappt/HabitList.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/HabitList.java
@@ -1,14 +1,43 @@
 package com.CMPUT301F21T19.habitappt;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class HabitList extends ArrayAdapter<Habit> {
 
-    public HabitList(@NonNull Context context, int resource) {
-        super(context, resource);
+    private ArrayList<Habit> habits;
+    private Context context;
+
+    public HabitList(Context context, ArrayList<Habit> habits) {
+        super(context,0, habits);
+        this.habits = habits;
+        this.context = context;
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+
+        if(view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.habit_list_item, parent,false);
+        }
+
+        Habit habit = habits.get(position);
+
+        TextView habitName = view.findViewById(R.id.habit_name);
+
+        habitName.setText(habit.getName());
+
+        return view;
+    }
 }
