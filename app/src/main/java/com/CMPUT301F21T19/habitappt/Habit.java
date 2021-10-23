c2c6f6cc6df6d572fb6f86134ca0b2ebe21b9868
AnnoyingCotton <andrewmjculberson@gmail.com>
2021-10-23 15:12:10 -0600

    Update Habit.java

--- app/src/main/java/com/CMPUT301F21T19/habitappt/Habit.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/Habit.java
@@ -7,4 +7,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Habit {
+    private String title;
+    private String reason;
+    private long dateToStart;
+
+
+    public Habit(String title,String reason,long dateToStart){
+        this.title = title;
+        this.reason = reason;
+        this.dateToStart = dateToStart;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public long getDateToStart() {
+        return dateToStart;
+    }
+
+    public void setDateToStart(long dateToStart) {
+        this.dateToStart = dateToStart;
+    }
 }
