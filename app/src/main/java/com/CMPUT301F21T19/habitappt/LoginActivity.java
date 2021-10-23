ffee805f70031dd512bd3b01a6fcf2d9704875c4
AnnoyingCotton <andrewmjculberson@gmail.com>
2021-10-23 14:38:10 -0600

    mock login page

---
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.CMPUT301F21T19.habitappt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Empty class for login screen
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    Button login_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(in);
+                finish();
+            }
+        });
+    }
+}
