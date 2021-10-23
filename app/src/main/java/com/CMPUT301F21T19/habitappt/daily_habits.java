3c5e273a4f2784934ce48a8daeed8d34ce658a42
loganv90 <60831705+loganv90@users.noreply.github.com>
2021-10-23 15:25:45 -0600

    add daily habits UI

--- app/src/main/java/com/CMPUT301F21T19/habitappt/daily_habits.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/daily_habits.java
@@ -2,63 +2,83 @@ package com.CMPUT301F21T19.habitappt;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link daily_habits#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class daily_habits extends Fragment {
+    ListView habitListView;
+    ArrayAdapter<Habit> habitAdapter;
+    ArrayList<Habit> habitDataList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    final String TAG = "Sample";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FirebaseFirestore db;
 
-    public daily_habits() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment daily_habits.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static daily_habits newInstance(String param1, String param2) {
-        daily_habits fragment = new daily_habits();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily_habits, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_daily_habits, container, false);
+
+        habitListView = view.findViewById(R.id.habit_list);
+
+        db = FirebaseFirestore.getInstance();
+
+        final CollectionReference collectionReference = db.collection("User Information").document("Default User").collection("Habits");
+
+        habitDataList = new ArrayList<>();
+        habitAdapter = new HabitList(getContext(), habitDataList);
+        habitListView.setAdapter(habitAdapter);
+
+        habitListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("what", "what1");
+            }
+        });
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                // Clear the old list
+                habitDataList.clear();
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
+                {
+                    Log.d(TAG, String.valueOf(doc.getData().get("Province Name")));
+                    String name = doc.getId();
+                    habitDataList.add(new Habit(name,"nothin",0)); // Adding the cities and provinces from FireStore
+                }
+                habitAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetch from the cloud
+            }
+        });
+
+        return view;
     }
 }
