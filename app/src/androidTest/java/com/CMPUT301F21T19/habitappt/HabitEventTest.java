1a9563e801a6313f88e29dfab61242d637205b6e
Abrar Hussain <77550655+abrarhs@users.noreply.github.com>
2021-11-02 17:46:54 -0600

    Test cases

---
+++ app/src/androidTest/java/com/CMPUT301F21T19/habitappt/HabitEventTest.java
@@ -0,0 +1,49 @@
+package com.CMPUT301F21T19.habitappt;
+
+import android.widget.EditText;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class HabitEventTest {
+
+        private Solo solo;
+
+        @Rule
+        public ActivityTestRule<LoginActivity> rule =
+                new ActivityTestRule<>(LoginActivity.class,true,true);
+
+        @Before
+        public void setUp() throws Exception{
+            solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+            solo.clickOnView(solo.getView(R.id.login_button));
+            solo.clickOnView(solo.getView(R.id.bottom_nav_bar));
+            solo.clickOnView(solo.getView(R.id.all_habit_button));
+            //solo.clickOnButton(R.id.all_habit_button);
+        }
+
+//    @Test
+//    public void start() throws Exception {
+//        Activity activity = rule.getActivity();
+//    }
+
+        @Test
+        public void addHabitEvent() {
+            //solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
+            solo.clickOnView(solo.getView(R.id.habit_list));
+            solo.clickOnView(solo.getView(R.id.add_event_button));
+            solo.enterText((EditText) solo.getView(R.id.event_comments),"Event Comment 1");
+            //add image (not yet done)
+            solo.clickOnView(solo.getView(R.id.event_date_calendar));
+            solo.setDatePicker(0, 2021, 10, 10);
+            solo.clickOnView(solo.getView(android.R.id.button1));
+            //solo.clickOnView(solo.getView(R.id.dialog_button));
+        }
+}
+
---
+++ app/src/androidTest/java/com/CMPUT301F21T19/habitappt/HabitTest.java
@@ -0,0 +1,73 @@
+package com.CMPUT301F21T19.habitappt;
+
+import android.app.Activity;
+import android.widget.EditText;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class HabitTest {
+        private Solo solo;
+
+        @Rule
+        public ActivityTestRule<LoginActivity> rule =
+                new ActivityTestRule<>(LoginActivity.class,true,true);
+
+        @Before
+        public void setUp() throws Exception{
+            solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+            solo.clickOnView(solo.getView(R.id.login_button));
+            solo.clickOnView(solo.getView(R.id.bottom_nav_bar));
+            solo.clickOnView(solo.getView(R.id.all_habit_button));
+        }
+
+        @Test
+        public void start() throws Exception {
+            Activity activity = rule.getActivity();
+        }
+
+        @Test
+        public void addHabit() {
+            solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
+            solo.clickOnView(solo.getView(R.id.add_habit_button));
+            solo.enterText((EditText) solo.getView(R.id.habit_title),"Habit1");
+            solo.enterText((EditText) solo.getView(R.id.habit_reason),"Reason1");
+            solo.clickOnButton("M");
+            solo.clickOnButton("W");
+            solo.clickOnButton("F");
+            solo.clickOnView(solo.getView(R.id.date_to_start));
+            solo.clickOnText("10");
+            solo.clickOnButton("Confirm");
+
+        }
+
+        @Test
+        public void editHabit() {
+            solo.clickOnView(solo.getView(R.id.habit_list));
+            solo.clickInList(0);
+            solo.clickOnView(solo.getView(R.id.edit_button));
+            solo.clearEditText((EditText) solo.getView(R.id.habit_title));
+            solo.enterText((EditText) solo.getView(R.id.habit_title),"New Habit_1");
+            solo.clearEditText((EditText) solo.getView(R.id.habit_reason));
+            solo.enterText((EditText) solo.getView(R.id.habit_reason),"New Reason_1");
+            solo.clickOnButton("M");
+            solo.clickOnButton("S");
+            solo.clickOnView(solo.getView(R.id.date_to_start));
+            solo.clickOnButton("Confirm");
+        }
+
+        @Test
+        public void deleteHabit() {
+            solo.clickOnView(solo.getView(R.id.habit_list));
+            solo.clickInList(0);
+            solo.clickOnView(solo.getView(R.id.edit_button));
+            solo.clickOnButton("Remove Habit");
+        }
+}
+
