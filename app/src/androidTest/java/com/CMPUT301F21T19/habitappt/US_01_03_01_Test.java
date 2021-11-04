eee288184a214b9677cff7cf3fdda9ac86b203c9
loganv90 <60831705+loganv90@users.noreply.github.com>
2021-11-04 12:49:19 -0600

    Test almost works

--- app/src/androidTest/java/com/CMPUT301F21T19/habitappt/US_01_03_01_Test.java
+++ app/src/androidTest/java/com/CMPUT301F21T19/habitappt/US_01_03_01_Test.java
@@ -1,6 +1,7 @@
 package com.CMPUT301F21T19.habitappt;
 
 import android.app.Activity;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
@@ -48,6 +49,10 @@ public class US_01_03_01_Test {
         solo.clickOnView(solo.getView(R.id.all_habit_button));
         solo.clickOnView(solo.getView(R.id.add_habit_button));
         solo.waitForText("Add Habit", 1, 2000);
+
+        //Fragment fragment = solo.getCurrentActivity().getFragmentManager().findFragmentById(R.id.
+        //EditText editText = (EditText) fragment.getView().findViewById(R.id.habit_title);
+
         solo.enterText((EditText) solo.getView(R.id.habit_title), "TestHabit-01-03-01");
         solo.enterText((EditText) solo.getView(R.id.habit_reason), "TestReason-01-03-01");
         solo.clickOnView(solo.getView(R.id.public_private_button));
@@ -57,9 +62,17 @@ public class US_01_03_01_Test {
 
         // Viewing the habit
         solo.clickOnText("TestHabit-01-03-01");
-        //solo.waitForText("TestHabit-01-03-01", 1, 2000);
-        //solo.waitForText("TestReason-01-03-01", 1, 2000);
-        //solo.waitForText("Private Habit", 1, 2000);
+        solo.waitForText("title:", 1, 2000);
+        solo.waitForText("TestHabit-01-03-01", 1, 2000);
+        solo.waitForText("reason:", 1, 2000);
+        solo.waitForText("TestReason-01-03-01", 1, 2000);
+        solo.waitForText("date:", 1, 2000);
+        solo.waitForText("10/", 1, 2000);
+        solo.waitForText("days:", 1, 2000);
+        // check days of week background colours
+        solo.waitForText("access:", 1, 2000);
+        solo.waitForText("Private Habit", 1, 2000);
+        // check private habit background colours
         solo.clickOnView(solo.getView(R.id.all_habit_button));
 
         // Removing the habit
@@ -67,90 +80,6 @@ public class US_01_03_01_Test {
         solo.clickOnView(solo.getView(R.id.edit_button));
         solo.clickOnButton("Remove Habit");
     }
-
-    /*
-    @Test
-    public void checkList() {
-        solo.assertCurrentActivity("Wrong Activity!", MainActivity.class);
-        solo.clickOnButton("ADD CITY");
-        solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
-        solo.clickOnButton("CONFIRM");
-        solo.clearEditText((EditText) solo.getView(R.id.editText_name));
-
-        assertTrue(solo.waitForText("Edmonton", 1, 2000));
-
-        solo.clickOnButton("ClEAR ALL");
-
-        assertFalse(solo.searchText("Edmonton"));
-    }
-
-    @Test
-    public void checkCityListItem() {
-        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        solo.clickOnButton("ADD CITY");
-        solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
-        solo.clickOnButton("CONFIRM");
-        solo.waitForText("Edmonton", 1, 2000);
-
-        MainActivity activity = (MainActivity) solo.getCurrentActivity();
-        final ListView cityList = activity.cityList;
-        String city = (String) cityList.getItemAtPosition(0);
-
-        assertEquals("Edmonton", city);
-    }
-
-    @Test
-    public void checkActivitySwitch() {
-        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        solo.clickOnButton("ADD CITY");
-        solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
-        solo.clickOnButton("CONFIRM");
-        solo.waitForText("Edmonton", 1, 2000);
-
-        MainActivity activity = (MainActivity) solo.getCurrentActivity();
-        final ListView cityList = activity.cityList;
-
-        solo.clickOnView(cityList.getChildAt(0));
-
-        assertTrue(solo.waitForActivity(showActivity.class, 2000));
-    }
-
-    @Test
-    public void checkCityName() {
-        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        solo.clickOnButton("ADD CITY");
-        solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
-        solo.clickOnButton("CONFIRM");
-        solo.waitForText("Edmonton", 1, 2000);
-
-        MainActivity activity = (MainActivity) solo.getCurrentActivity();
-        final ListView cityList = activity.cityList;
-
-        solo.clickOnView(cityList.getChildAt(0));
-        solo.waitForActivity(showActivity.class, 2000);
-
-        assertEquals("Edmonton", ((TextView) solo.getView(R.id.city_text_view)).getText().toString());
-    }
-
-    @Test
-    public void checkBackButton() {
-        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        solo.clickOnButton("ADD CITY");
-        solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
-        solo.clickOnButton("CONFIRM");
-        solo.waitForText("Edmonton", 1, 2000);
-
-        MainActivity activity = (MainActivity) solo.getCurrentActivity();
-        final ListView cityList = activity.cityList;
-
-        solo.clickOnView(cityList.getChildAt(0));
-        solo.waitForActivity(showActivity.class, 2000);
-        solo.clickOnButton("BACK");
-
-        assertTrue(solo.waitForActivity(MainActivity.class, 2000));
-        assertTrue(solo.searchText("Edmonton"));
-    }
-    */
 
     @After
     public void tearDown() {
--- app/src/main/java/com/CMPUT301F21T19/habitappt/all_habits.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/all_habits.java
@@ -6,6 +6,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
