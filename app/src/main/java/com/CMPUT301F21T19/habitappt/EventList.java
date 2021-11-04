256e13005ae42c1d22e817a91f6955a5d974b918
Hamzah <hamzah@ualberta.ca>
2021-11-04 15:24:44 -0600

    completed eventList javadoc

--- app/src/main/java/com/CMPUT301F21T19/habitappt/EventList.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/EventList.java
@@ -15,19 +15,35 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * represents list of events for associated users habits, inherits array adapter
+ */
 public class EventList extends ArrayAdapter<HabitEvent> {
 
+    /**
+     * actual data structure that holds events for given users habits
+     */
     private ArrayList<HabitEvent> events;
     private Context context;
 
+    /**
+     * constructs eventlist given passedcontext and list of events
+     * @param context
+     * @param events
+     */
     public EventList(@NonNull Context context, ArrayList<HabitEvent> events) {
         super(context, 0, events);
         this.events = events;
         this.context = context;
     }
 
-
-
+    /**
+     * creates custom array adapter view for events
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return view
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
