0c08acd6a5f7ee6c8d34789d75ee4353d9dea453
Abrar Hussain <77550655+abrarhs@users.noreply.github.com>
2021-11-05 13:56:49 -0600

    Habit Event edit/delete tests, javadoc for nav_bar

--- app/src/androidTest/java/com/CMPUT301F21T19/habitappt/HabitEventTest.java
+++ app/src/androidTest/java/com/CMPUT301F21T19/habitappt/HabitEventTest.java
@@ -1,8 +1,10 @@
 package com.CMPUT301F21T19.habitappt;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import android.app.Activity;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -10,6 +12,7 @@ import androidx.test.rule.ActivityTestRule;
 
 import com.robotium.solo.Solo;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -52,7 +55,7 @@ public class HabitEventTest {
         }
 
         @Test
-        public void characterlimit(){
+        public void eventcharacterlimit(){
             solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
 
             solo.clickOnView(solo.getView(R.id.all_habit_button));
@@ -61,10 +64,8 @@ public class HabitEventTest {
 
             solo.clickOnView(solo.getView(R.id.add_event_button));
             solo.clearEditText((EditText) solo.getView(R.id.event_comments));
-            solo.enterText((EditText) solo.getView(R.id.event_comments),"20characterlimitttt");
-            solo.clickOnView(solo.getView(R.id.event_date_calendar));
-            solo.clickOnButton("Confirm");
-            assertTrue(solo.waitForText("20characterlimitttt", 1,5000 ));
+            solo.enterText((EditText) solo.getView(R.id.event_comments),"20characterlimitttttttt");
+            solo.sleep(3000);
         }
 
         @Test
@@ -76,8 +77,59 @@ public class HabitEventTest {
             solo.clickInList(0);
 
             solo.sleep(5000);
+        }
+        @Test
+        public void editHabitEvent(){
+            solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
 
+            solo.clickOnView(solo.getView(R.id.all_habit_button));
+            solo.clickOnText("Test Habit Event");
+            int fromX, toX, fromY, toY;
+            int[] location = new int[2];
+
+            View row = solo.getText("New Event");
+            row.getLocationInWindow(location);
+
+            fromX = location[0] + 200;
+            fromY = location[1];
+            toX = location[0];
+            toY = fromY;
+            solo.drag(fromX, toX, fromY, toY, 10);
+
+            solo.sleep(1000);
+            solo.clickOnText("Edit");
+            solo.clearEditText((EditText) solo.getView(R.id.event_comments));
+            solo.enterText((EditText) solo.getView(R.id.event_comments),"New Event 1");
+            solo.clickOnView(solo.getView(R.id.event_date_calendar));
+            solo.clickOnButton("Confirm");
+            assertTrue(solo.waitForText("New Event 1"));
 
         }
+        @Test
+        public void deleteHabitEvent(){
+            solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
+
+            solo.clickOnView(solo.getView(R.id.all_habit_button));
+            solo.clickOnText("Test Habit Event");
+
+            int fromX, toX, fromY, toY;
+            int[] location = new int[2];
+            View row = solo.getText("New Event");
+            row.getLocationInWindow(location);
+
+            fromX = location[0] + 200;
+            fromY = location[1];
+            toX = location[0];
+            toY = fromY;
+            solo.drag(fromX, toX, fromY, toY, 10);
+
+            solo.sleep(1000);
+            solo.clickOnImage(R.drawable.ic_delete);
+            solo.clickOnButton("Remove Habit");
+        }
+
+        @After
+        public void tearDown() { solo.finishOpenedActivities(); }
+
 }
 
