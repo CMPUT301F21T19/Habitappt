5e9ff14e38a8a38d04cb5c7b4e4c1e248cc7f5c7
Hamzah <hamzah@ualberta.ca>
2021-11-29 07:37:51 -0700

    added javadoc to ViewEvent

--- app/src/main/java/com/CMPUT301F21T19/habitappt/Fragments/ViewEvent.java
+++ app/src/main/java/com/CMPUT301F21T19/habitappt/Fragments/ViewEvent.java
@@ -1,13 +1,18 @@
+/**
+ * Copyright 2021 - 2021 CMPUT301F21T19 (Habitappt). All rights reserved. This document nor any
+ * part of it may be reproduced, stored in a retrieval system or transmitted in any for or by any
+ * means without prior permission of the members of CMPUT301F21T19 or by the professor and any
+ * authorized TAs of the CMPUT301 class at the University of Alberta, fall term 2021.
+ *
+ * Class: ViewEvent
+ *
+ * Description: Views a habit event and its associated details.
+ */
 package com.CMPUT301F21T19.habitappt.Fragments;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,24 +21,24 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.CMPUT301F21T19.habitappt.Entities.HabitEvent;
 import com.CMPUT301F21T19.habitappt.Entities.User;
 import com.CMPUT301F21T19.habitappt.R;
 import com.CMPUT301F21T19.habitappt.Utils.SharedHelper;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-/**
- * This fragment is used to view a habit event and its associated details.
- */
+
 public class ViewEvent extends Fragment {
 
     private HabitEvent event;
@@ -89,6 +94,7 @@ public class ViewEvent extends Fragment {
 
         eventImageView.setImageBitmap(event.getImg());
         dateTextView.setText(SharedHelper.getStringDateFromLong(event.getEventDate()));
+
         //locationTextView stuff here when location is done
         commentTextView.setText(event.getComment());
 
